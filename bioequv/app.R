34a24916a3e9dd04f3a296d73b58d1cad0b7dd1e
unknown <eamonnjobrien@gmail.com>
2019-11-09 00:53:45 +0000

    all workig

--- bioequv/app.R
+++ bioequv/app.R
@@ -19,9 +19,14 @@
     lines <- c("1 31 B REF 5.957211 1 31 M REF 5.961802 1 31 E REF 5.967178 1 32 B REF 6.010251 1 32 M REF 6.004711 1 32 E REF 6.004797 1 33 B REF 5.884161 1 33 M REF 5.894085 1 33 E REF 5.895977 1 34 B REF 5.624705 1 34 M REF 5.632991 1 34 E REF 5.614428 1 35 B REF 5.957329 1 35 M REF 5.966059 1 35 E REF 5.968143 1 36 B REF 5.074298 1 36 M REF 5.063063 1 36 E REF 5.058519 1 37 B REF 5.418587 1 37 M REF 5.420591 1 37 E REF 5.418178 1 38 B REF 6.325178 1 38 M REF 6.321954 1 38 E REF 6.303148 1 39 B REF 5.656286 1 39 M REF 5.68025 1 39 E REF 5.675036 1 40 B REF 5.792299 1 40 M REF 5.775161 1 40 E REF 5.793083 2 41 B REF 5.601033 2 41 M REF 5.611223 2 41 E REF 5.601142 2 42 B REF 5.61553 2 42 M REF 5.587412 2 42 E REF 5.591004 2 43 B REF 5.682466 2 43 M REF 5.676472 2 43 E REF 5.671434 2 44 B REF 5.844336 2 44 M REF 5.855172 2 44 E REF 5.862329 2 45 B REF 5.898151 2 45 M REF 5.883657 2 45 E REF 5.878956 2 46 B REF 6.100662 2 46 M REF 6.105463 2 46 E REF 6.108098 2 47 B REF 6.294753 2 47 M REF 6.28534 2 47 E REF 6.302333 2 48 B REF 5.638072 2 48 M REF 5.627372 2 48 E REF 5.623516 2 49 B REF 5.113562 2 49 M REF 5.122454 2 49 E REF 5.109271 2 50 B REF 5.932752 2 50 M REF 5.913438 2 50 E REF 5.912427 3 51 B REF 5.961947 3 51 M REF 5.955332 3 51 E REF 5.943721 3 52 B REF 6.2334 3 52 M REF 6.250689 3 52 E REF 6.219668 3 53 B REF 6.041431 3 53 M REF 6.038234 3 53 E REF 6.080464 3 54 B REF 6.049713 3 54 M REF 6.039759 3 54 E REF 6.054218 3 55 B REF 6.834563 3 55 M REF 6.85264 3 55 E REF 6.857395 3 56 B REF 4.864966 3 56 M REF 4.907521 3 56 E REF 4.891049 3 57 B REF 5.895176 3 57 M REF 5.885851 3 57 E REF 5.874895 3 58 B REF 6.45826 3 58 M REF 6.443113 3 58 E REF 6.435882 3 59 B REF 6.090533 3 59 M REF 6.102835 3 59 E REF 6.077606  3 60 B REF 5.886724 3 60 M REF 5.920949 3 60 E REF 5.915749 4 1 B TEST 6.894594 4 1 M TEST 6.913011 4 1 E TEST 6.895764 4 2 B TEST 5.832334 4 2 M TEST 5.846562 4 2 E TEST 5.832269 4 3 B TEST 6.235755 4 3 M TEST 6.26231 4 3 E TEST 6.245095 4 4 B TEST 5.646185 4 4 M TEST 5.635887 4 4 E TEST 5.63034 4 5 B TEST 5.960711 4 5 M TEST 5.962902 4 5 E TEST 5.961959 4 6 B TEST 5.500354 4 6 M TEST 5.508444 4 6 E TEST 5.513115 4 7 B TEST 6.663099 4 7 M TEST 6.64733 4 7 E TEST 6.651215 4 8 B TEST 5.724774 4 8 M TEST 5.72086 4 8 E TEST 5.71411 4 9 B TEST 6.183375 4 9 M TEST 6.186433 4 9 E TEST 6.182109 4 10 B TEST 5.64053 4 10 M TEST 5.648589 4 10 E TEST 5.626395 5 11 B TEST 6.69764 5 11 M TEST 6.71128 5 11 E TEST 6.699829 5 12 B TEST 6.555609 5 12 M TEST 6.549935 5 12 E TEST 6.551611 5 13 B TEST 5.009683 5 13 M TEST 5.013969 5 13 E TEST 5.010928 5 14 B TEST 5.440976 5 14 M TEST 5.42057 5 14 E TEST 5.447687 5 15 B TEST 6.477609 5 15 M TEST 6.456082  5 15 E TEST 6.448981 5 16 B TEST 6.442601 5 16 M TEST 6.426217 5 16 E TEST 6.436262 5 17 B TEST 5.640496 5 17 M TEST 5.63846 5 17 E TEST 5.640755 5 18 B TEST 6.597718 5 18 M TEST 6.599232 5 18 E TEST 6.609437 5 19 B TEST 6.007241 5 19 M TEST 5.990695 5 19 E TEST 5.984292 5 20 B TEST 6.781806 5 20 M TEST 6.774386 5 20 E TEST 6.784001 6 21 B TEST 5.993852 6 21 M TEST 5.994287 6 21 E TEST 5.993541 6 22 B TEST 6.012322 6 22 M TEST 6.006182 6 22 E TEST 6.017961 6 23 B TEST 5.965969 6 23 M TEST 5.97125 6 23 E TEST 5.967839 6 24 B TEST 5.592609 6 24 M TEST 5.581154 6 24 E TEST 5.588877 6 25 B TEST 6.002182 6 25 M TEST 6.011583 6 25 E TEST 6.018746 6 26 B TEST 5.267014 6 26 M TEST 5.272291 6 26 E TEST 5.265213 6 27 B TEST 5.766104 6 27 M TEST 5.786727 6 27 E TEST 5.773194 6 28 B TEST 6.054975 6 28 M TEST 6.05232 6 28 E TEST 6.061088 6 29 B TEST 5.838689 6 29 M TEST 5.837566 6 29 E TEST 5.842508 6 30 B TEST 5.784255 6 30 M TEST 5.789891 6 30 E TEST 5.788662")
     con <- textConnection(lines)
     cnames <- c("BATCH","SECTOR","REP","PRODUCT","y")
-    fda <- read.table(con, col.names=cnames)
+    fda.d <- read.table(con, col.names=cnames)
     close(con)
 
+    fda2 <- fda.d
+    fda2$fda1 <- exp(fda2$y)  # the bioequiv function logs the data, but the fda data is already logged
+    
+    
+    
 # inputs
 # here is my function for performing the analysis
 # data
@@ -482,9 +487,9 @@ see notes tab for more information. You also have the choices of selecting a new
                     #~~~~~~~~~~~~~
                     tabPanel("2c List the FDA data", 
                              
-                             div( verbatimTextOutput("summary3")),
+                             div( verbatimTextOutput("bioequivfda2")), #
                              
-                             p(strong("copy the above data to do your own analysis................."))
+                            p(strong("copy the above data to do your own analysis................."))
                              
                     ),
                     #~~~~~~~~~~~~~~~~~~
@@ -884,10 +889,9 @@ server <- shinyServer(function(input, output) {
     
     output$reg.plot2 <- renderPlot({         
         
-        # Get the current regression data
-        data1 <- bioequivfda()
+        # Get the fda  data
         
-        d1 <- data1$foo
+        d1<-fda.d
         
         # Conditionally plot
         if (input$Plot == "Base plot") {
@@ -972,19 +976,32 @@ server <- shinyServer(function(input, output) {
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
-    bioequivfda <- reactive({
+
+    
+    
+    bioequivfdax <- reactive({
+  
       
-        foo <- fda
-        
-        foo$fda1 <- exp(foo$y)  # the bioequiv function logs the data, but the fda data is already logged
-        
-        res.fdax <- bioequiv(foo1=foo , nrXlr=10*3, mr= 3, 
-                                       ntXlt=10*3, mt= 3,
-                 response="fda1",indep="SECTOR", split="PRODUCT", ref="REF", test="TEST")
-        
-        return(list(res.fda=res.fdax, foo=foo))
-        
-        
+      foo8 <- fda.d
+      
+      foo8$fda1 <- exp(foo8$y)  # the bioequiv function logs the data, but the fda data is already logged
+      
+      
+      res.fda <- bioequiv(foo1=foo8 , nrXlr=10*3, mr= 3, 
+                          ntXlt=10*3, mt= 3,
+                          response="fda1",indep="SECTOR", split="PRODUCT", ref="REF", test="TEST")
+      
+      return(res.fda)
+      
+      
+    })
+    
+    fda.data <- reactive({
+      
+      foo99 <- fda2
+      
+      return(foo99)
+      
     })
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -1054,21 +1071,35 @@ server <- shinyServer(function(input, output) {
         
     })
     
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+    # print the fda data
+    output$bioequivfda2 <- renderPrint({
+      
+      return(fda2)
+      
+    })
+    
     #~~~~~~~~~~~analysis on fda data
      output$bioequivfda1 <- renderPrint({
         
-        return(bioequivfda()$res.fdax)
-       
+      return(bioequivfdax()$res.fda)
+
    })
+     
+     
+     # output$bioequivfda1 <- renderPrint({
+     #   
+     #   foo <- fda
+     #   
+     #   foo$fda1 <- exp(foo$y)  # the bioequiv function logs the data, but the fda data is already logged
+     #   
+     #   res.fdax <- bioequiv(foo1=foo , nrXlr=10*3, mr= 3, 
+     #                        ntXlt=10*3, mt= 3,
+     #                        response="fda1",indep="SECTOR", split="PRODUCT", ref="REF", test="TEST")
+     #   
+     #   })
   
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-    # print the fda data
-    output$summary3 <- renderPrint({
-        
-        return(bioequivfda()$foo)
-        
-    })
-    
+   
     #~~~~~~~~~~~ loading in data
     output$contents <- renderTable({
         
